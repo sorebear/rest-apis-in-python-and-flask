f5507507c97c57dbd6ef8938173f193f8625b289
sorebear <sorenbaird@gmail.com>
2018-01-09 20:35:06 -0800

    Updated Python version in VS settings

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,44 @@
 from flask import Flask
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'Wally\'s Store',
+        'items': [
+            {
+                'name': 'Brocolli',
+                'price': .99
+            }
+        ]
+    }
+]
 
-@app.route('/') #http://www.google.com/
-def home():
-    return "Hello, world!"
+# POST - used to receive data
+# GET - used to send data back only
 
-@app.route('/mcdonalds')
-def mcdonalds():
-    return "Can I get a McPick Two!?"
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>') 
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store():
+    pass
 
 app.run(port=5000)
