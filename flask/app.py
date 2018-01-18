f1daaf40e7e376cd68fbcc19d81f4abc22a8c641
Soren Baird <sorenbaird@gmail.com>
2018-01-17 17:43:15 -0800

    Added jsonify to method return

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+# JSON is basically a Dictionary - See Below
 stores = [
     {
         'name': 'Wally\'s Store',
@@ -29,7 +30,7 @@ def get_store(name):
 # GET /store
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
